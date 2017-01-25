61cd52eeeb1424cc6b29f19a5a1d74265a1f8126
Kevin John Benson <benson_k3@olin217f.mathsci.denison.edu>
2017-01-24 19:40:15 -0500

    disaster of a proj

---
+++ Assets/TitleGUI.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var customSkin:GUISkin;
+
+function OnGUI () {
+	GUI.skin = customSkin;
+	if(GUI.Button(Rect(0,0,100,50),"Play Game")) {
+		print("Button Clicked!");
+	}
+
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
